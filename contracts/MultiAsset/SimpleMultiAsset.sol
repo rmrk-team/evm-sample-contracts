7f13b2e9546148444ba13da31833afa739d5980a
Jan Turk <jan.turk12@gmail.com>
2022-12-12 01:01:47 +0100

    Update MultiAsset

--- contracts/MultiAsset/SimpleMultiAsset.sol
+++ contracts/MultiAsset/SimpleMultiAsset.sol
@@ -5,16 +5,13 @@ import "@rmrk-team/evm-contracts/contracts/implementations/RMRKMultiAssetImpl.so
 
 contract SimpleMultiAsset is RMRKMultiAssetImpl {
     // NOTE: Additional custom arguments can be added to the constructor based on your needs.
-    constructor(uint256 maxSupply, uint256 pricePerMint)
+    constructor(InitData memory data)
         RMRKMultiAssetImpl(
             "SimpleMultiAsset",
             "SMA",
-            maxSupply,
-            pricePerMint,
             "ipfs://meta",
             "ipfs://tokenMeta",
-            msg.sender,
-            10
+            data
         )
     {}
 }
